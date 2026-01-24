711405ecfec321ecfa3f52c31b76ea4b830f32c5
Sodel <delaude.so@gmail.com>
2026-01-25 00:38:26 +0100

    feat: add RoomComponent with routing and template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/page/home.component';
+import { RoomComponent } from '../room/room.component';
 
 export enum AppPath {
     Home = '',
     Rooms = 'rooms',
 }
 
+export enum AppParam {
+    RoomId = 'roomId',
+}
+
 export const routes: Routes = [
     {
         path: AppPath.Home,
         component: HomeComponent,
+    },
+    {
+        path: AppPath.Rooms + '/:' + AppParam.RoomId,
+        component: RoomComponent,
     },
     {
         path: '**',
---
+++ src/room/room.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room',
+  imports: [],
+  templateUrl: './room.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RoomComponent {
+
+}
